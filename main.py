051f3f6c15cd8cd4b02ca37f61ea4bc7b2502812
DarkSideDani <darkside.dascal6@gmail.com>
2025-08-10 20:36:48 +0100

    Implemented sprites and the player with it's own hitbox (circle)

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
-    running = True
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    dt = 0
     
+    running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip() # display update
+        dt = clock.tick(60) / 1000 # limit the FPS to 60
+        
+
 
 
 if __name__ == "__main__":

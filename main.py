3676fa1e1ca60333dfc0a9654893e115545520d4
DarkSideDani <darkside.dascal6@gmail.com>
2025-08-10 21:11:11 +0100

    Implemented the starting picture with teh name of the game

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import pygame
+import time
 from constants import *
 from player import Player
 
+def show_splash(screen):
+    splash = pygame.image.load("Astroroids.png")
+    splash = pygame.transform.scale(splash, (1280,720))
+    screen.fill((0,0,0))
+    screen.blit(splash, (0,0))
+    pygame.display.flip()
+    
+    start_time = time.time()
+    while time.time() - start_time < 5:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    show_splash(screen)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     

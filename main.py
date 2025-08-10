d712705cff68bce8decadc656d78d7d699b6eca1
DarkSideDani <darkside.dascal6@gmail.com>
2025-08-10 20:08:09 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip() # display update
+
+
+if __name__ == "__main__":
+    main()
